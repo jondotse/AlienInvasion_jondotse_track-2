aafbebb6be03a63998222857ca2850b3d2dfc84d
JonD <sethdotse05@gmail.com>
2026-08-02 22:02:05 -0400

    Add docstrings to game_stats.py - header.

---
+++ game_stats.py
@@ -0,0 +1,84 @@
+# from pathlib import Path
+"""
+Program Name: game_stats.py
+Author: Jonathan Dotse
+Purpose: Defines the GameStats class, which tracks the player's current score
+ships remaining, and level during gameplay.
+Starter Code: Based on the Alien Invasion tutorial starter code, originally cloned from
+    https://github.com/jondotse/alien_Invasion_starter.git
+Date: August 2, 2026
+"""
+import json
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
+
+class GameStats():
+
+    def __init__(self, game: 'AlienInvasion'):
+        self.game = game
+        self.settings = game.settings
+        self.max_score = 0
+        self.init_saved_scores()
+        self.reset_stats()
+
+    def init_saved_scores(self):
+        self.path = self.settings.scores_file 
+        if self.path.exists() and self.path.stat().st_size > 0:
+            contents = self.path.read_text()
+            if not contents:
+                print('file empty')
+            scores = json.loads(contents)
+            self.hi_score = scores.get('hi_score', 0)
+        else:
+            self.hi_score = 0
+            self.save_scores()
+            # save the file
+
+    def save_scores(self):
+        scores = {
+            'hi_score': self.hi_score
+        }
+        contents = json.dumps(scores, indent=4)
+        try:
+            self.path.write_text(contents)
+        except FileNotFoundError as e:
+            print(f'File Not Found: {e}')
+
+    def reset_stats(self):
+        self.ships_left = self.settings.starting_ship_count
+        self.score = 0
+        self.level = 1
+
+
+    def update(self, collisions):
+        # update score
+        self._update_score(collisions)
+        # update max_score
+        self._update_max_score()
+
+        # update hi_score
+        self._update_hi_score()
+
+    def _update_max_score(self):
+        if self.score > self.max_score:
+            self.max_score = self.score
+            # print(f'Max: {self.max_score}')
+    def _update_hi_score(self):
+        if self.score > self.hi_score:
+            self.hi_score = self.score
+            # print(f'Hi: {self.max_score}')
+
+    def _update_score(self, collisions):
+        for alien in collisions.values():
+            self.score += self.settings.alien_points
+        # print(f'Basic: {self.score}')
+
+    def update_level(self):
+        self.level += 1
+        #print(self.level)
+
+       
